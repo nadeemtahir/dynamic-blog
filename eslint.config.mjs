cde3b661899b4fdfed0628d5ba4d2123a715c2a5
nadeemtahir <163245288+nadeemtahir@users.noreply.github.com>
2024-12-27 06:33:51 -0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/quotes": "off",
+      "quotes": [0],
+    },
+  },
 ];
 
 export default eslintConfig;
